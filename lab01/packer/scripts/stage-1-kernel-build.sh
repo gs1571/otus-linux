53615fdfbde5c62b1e4f3085da1a1ffbfcf57450
Grigory Solovyev <gs1571@gmail.com>
2019-11-10 14:21:29 +0000

    lab01: home work with ** is done

--- lab01/packer/scripts/stage-1-kernel-build.sh
+++ lab01/packer/scripts/stage-1-kernel-build.sh
@@ -8,7 +8,7 @@
 # rm -f /boot/*3.10*
 
 # install required packets
-yum install -y ncurses-devel make gcc bc bison flex elfutils-libelf-devel openssl-devel grub2 wget perl
+yum install -y ncurses-devel make gcc bc bison flex elfutils-libelf-devel openssl-devel grub2 wget perl bzip2
 # download and extract sources
 cd /usr/src/ 
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.3.8.tar.xz 
@@ -19,12 +19,12 @@ cp -v /boot/config-* .config
 # create config
 make olddefconfig
 # compile and install kernel
-date && make -j3 && sleep 2m && date && make -j3 modules && sleep 2m && date && make modules_install && sleep 2m && date && make install && date
+date && make -j12 && sleep 1m && date && make -j12 modules && sleep 1m && date && make modules_install && sleep 1m && date && make install && date
 # Update GRUB
 grub2-mkconfig -o /boot/grub2/grub.cfg
 grub2-set-default 0
 echo "Grub update done."
-sleep 2m 
+sleep 1m 
 # Reboot VM
 shutdown -r now
 echo "Going to reboot"
