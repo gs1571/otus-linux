3236d677fce179b4d78d779cc2292e7c6602085b
Grigorii Solovev <gsolovev@luxoft.com>
2019-12-03 08:05:58 +0300

    Lab06: begin writing script

--- lab06/httpdlog_analyzer.sh
+++ lab06/httpdlog_analyzer.sh
@@ -1,4 +1,10 @@
 logFile=./access.log
-cat $logFile | awk '/GET \/ HTTP/{ ipcount[$1]++ } END { for (i in ipcount) { printf "IP:%13s - %d times\n", i, ipcount[i] } }' | sort -rn | head -20
+X=10
+Y=10
 
-cat $logFile | awk '/GET \/ HTTP/{ ipcount[$1]++ } END { for (i in ipcount) { printf "IP:%13s - %d times\n", i, ipcount[i] } }' | sort -rn 
+echo "TOP $X active IPs:"
+cat $logFile | awk '/GET/{ iprequests[$1]++ } END { for (i in iprequests) { printf "%4d - IP:%13s\n", iprequests[i], i } }' | sort -rn | head -$X
+echo "TOP $X requested locations:"
+cat $logFile | awk '/GET/{ locations[$7]++ } END { for (i in locations) { printf "%4d - location:%s\n", locations[i], i } }' | sort -rn | head -$X
+
+cat $logFile | awk '{ print "date: " $4 ; if ( $9 ~ /[0-9]/ ) { print "code: " $9 } else { print "correct code: " $7 } }'
