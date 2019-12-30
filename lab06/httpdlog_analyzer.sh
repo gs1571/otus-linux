670f9734899d9657aecfd03c8af9ec70f4209f76
Grigorii Solovev <gsolovev@luxoft.com>
2019-12-30 10:36:14 +0300

    lab06: some updates to scripts

--- lab06/httpdlog_analyzer.sh
+++ lab06/httpdlog_analyzer.sh
@@ -2,9 +2,17 @@ logFile=./access.log
 X=10
 Y=10
 
+#get_http_code {
+#}
+
 echo "TOP $X active IPs:"
 cat $logFile | awk '/GET/{ iprequests[$1]++ } END { for (i in iprequests) { printf "%4d - IP:%13s\n", iprequests[i], i } }' | sort -rn | head -$X
 echo "TOP $X requested locations:"
 cat $logFile | awk '/GET/{ locations[$7]++ } END { for (i in locations) { printf "%4d - location:%s\n", locations[i], i } }' | sort -rn | head -$X
+echo "Error codes:"
+cat $logFile | awk 'BEGIN{FS="\""}{print $3}' | awk 'BEGIN{FIELDWIDTHS="1 3"}{if ($1 < 600 && $1 > 399) print $1}' | sort -u | sort
+echo "All HTTP codes:"
+cat $logFile | awk 'BEGIN{FS="\""}{print $3}' | awk 'BEGIN{FIELDWIDTHS="1 3"}{ codes[$1]++ } END { for (i in codes) { printf "%4d - %3d\n", codes[i], i } }' | sort -rn
 
-cat $logFile | awk '{ print "date: " $4 ; if ( $9 ~ /[0-9]/ ) { print "code: " $9 } else { print "correct code: " $7 } }'
+#cat http-status-codes.csv | awk '/^[[:digit:]]+,/BEGIN{FS=","}{print $1,$2}'
+#awk '/^[[:digit:]]+,/BEGIN{FS=","}{print $1,$2}' http-status-codes.csv
