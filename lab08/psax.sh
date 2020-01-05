eec555afdf14bda2427cf77290937d643c14941a
Grigorii Solovev <gsolovev@luxoft.com>
2020-01-05 21:10:31 +0300

    Lab08: psax.sh update calculation of cpu_time and verify application bc

--- lab08/psax.sh
+++ lab08/psax.sh
@@ -1,3 +1,13 @@
+# cheking that bc is installed
+if ! which bc &> /dev/null
+then
+    echo "The script require bc (an arbitrary precision calculator language), please, install it before:"
+    echo "  sudo yum install bc"
+    echo "or"
+    echo "  sudo apt install bc"
+    exit 1
+fi
+
 # get cputime
 cputime=$(head -n 1 /proc/stat | awk '{print $2}')
 printf "%5s %-8s %-6s %4s %-20s\n" PID TTY STAT TIME COMMAND
@@ -8,7 +18,8 @@ printf "%5s %-8s %-6s %4s %-20s\n" PID TTY STAT TIME COMMAND
 
 # follow the instruction https://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat
 # get HZ and uptime
-hz=$(grep 'CONFIG_HZ=' /boot/config-$(uname -r) | sed 's/CONFIG_HZ=//')
+#hz=$(grep 'CONFIG_HZ=' /boot/config-$(uname -r) | sed 's/CONFIG_HZ=//')
+clk_tck=$(getconf CLK_TCK)
 uptime=$(cat /proc/uptime | awk '{ print $1 }' | awk -F. '{ print $1 }')
 
 # just for debug
@@ -20,7 +31,7 @@ do
     if [[ $item =~ ^-?[0-9]+$ ]]
     then
         # check terminal
-        if [ -e /proc/$item/fd/0 ]
+        if [[ -e /proc/$item/fd/0 ]]
         then
             # get tty
             tty=$(ls -l /proc/$item/fd/0 | awk '{print $11}')
@@ -37,33 +48,40 @@ do
             tty="?"
         fi
         # check stat file
-        if [ -e /proc/$item/stat ]
+        if [[ -e /proc/$item/stat ]]
         then
             # get status
             stat=$(cat /proc/$item/stat | awk '{print $3}')
             # follow the instruction https://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat
-            # get utime, stime, cutime, cstime, starttime
+            # get utime, stime, starttime
             utime=$(cat /proc/$item/stat | awk '{ print $14 }')
             stime=$(cat /proc/$item/stat | awk '{ print $15 }')
-            cutime=$(cat /proc/$item/stat | awk '{ print $16 }')
-            cstime=$(cat /proc/$item/stat | awk '{ print $17 }')
             starttime=$(cat /proc/$item/stat | awk '{ print $22 }')
-
-            total_time=$[utime+stime+cutime+cstime]
-            seconds=$[uptime-(starttime/hz)]
-            cpu_usage=$[100*((total_time/hz)/seconds)]
+            # line calculator  - bc will be used
+            proc_total_time=$( echo "scale=2; $utime / $clk_tck + $stime / $clk_tck" | bc )
+            time_since_proc_started=$( scale=2; echo "$uptime - $starttime / $clk_tck" | bc )
+            # since uptime in seconds for just runned process we get zero seconds, in that case cpu usage will be equal zero
+            if [[ time_since_proc_started -ne 0 ]] 
+            then
+                cpu_usage=$( echo "scale=2; 100 * $proc_total_time / $time_since_proc_started" | bc )
+            else
+                cpu_usage=0
+            fi
         # if check file doesn't exist, zeroize varibales
         else
             stat="?"
             cpu_usage=0
         fi
         # check command
-        if [ -e /proc/$item/cmdline ]
+        if [[ -e /proc/$item/cmdline ]]
         then
             cmd=$(cat /proc/$item/cmdline)
-        else
-            cmd=""
         fi
+        if [[ -z $cmd ]] && [[ -e /proc/$item/comm ]]
+        then
+            comm=$(cat /proc/$item/comm)
+            cmd=$(echo "[" $comm "]")
+        fi  
 
         printf "%5d %-8s %-6s %1.2f %-20s\n" $item $tty $stat $cpu_usage "$cmd"
     fi
