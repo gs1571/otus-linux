7f9b4c72109164280f5e2c777b658ce0a42c10d9
Grigorii Solovev <gsolovev@luxoft.com>
2020-01-05 21:32:02 +0300

    Lab08: psax.sh updated calculation for time of process, because it is not percent, but time

--- lab08/psax.sh
+++ lab08/psax.sh
@@ -8,19 +8,24 @@ then
     exit 1
 fi
 
+convertsecs() {
+ ((h=${1}/3600))
+ ((m=(${1}%3600)/60))
+ ((s=${1}%60))
+ printf "%02d:%02d:%02d" $h $m $s
+}
+
 # get cputime
 cputime=$(head -n 1 /proc/stat | awk '{print $2}')
-printf "%5s %-8s %-6s %4s %-20s\n" PID TTY STAT TIME COMMAND
+printf "%5s %-8s %-6s%9s %-20s\n" PID TTY STAT TIME COMMAND
 
 # just for debug
 # range=(1237 1310 1395 26981 26862)
 # range=(1938 1951)
 
 # follow the instruction https://stackoverflow.com/questions/16726779/how-do-i-get-the-total-cpu-usage-of-an-application-from-proc-pid-stat
-# get HZ and uptime
 #hz=$(grep 'CONFIG_HZ=' /boot/config-$(uname -r) | sed 's/CONFIG_HZ=//')
 clk_tck=$(getconf CLK_TCK)
-uptime=$(cat /proc/uptime | awk '{ print $1 }' | awk -F. '{ print $1 }')
 
 # just for debug
 #for item in "${range[@]}"
@@ -56,17 +61,9 @@ do
             # get utime, stime, starttime
             utime=$(cat /proc/$item/stat | awk '{ print $14 }')
             stime=$(cat /proc/$item/stat | awk '{ print $15 }')
-            starttime=$(cat /proc/$item/stat | awk '{ print $22 }')
-            # line calculator  - bc will be used
-            proc_total_time=$( echo "scale=2; $utime / $clk_tck + $stime / $clk_tck" | bc )
-            time_since_proc_started=$( scale=2; echo "$uptime - $starttime / $clk_tck" | bc )
-            # since uptime in seconds for just runned process we get zero seconds, in that case cpu usage will be equal zero
-            if [[ time_since_proc_started -ne 0 ]] 
-            then
-                cpu_usage=$( echo "scale=2; 100 * $proc_total_time / $time_since_proc_started" | bc )
-            else
-                cpu_usage=0
-            fi
+            # line calculator - bc will be used
+            proc_total_time=$( echo "$utime / $clk_tck + $stime / $clk_tck" | bc )
+
         # if check file doesn't exist, zeroize varibales
         else
             stat="?"
@@ -83,7 +80,9 @@ do
             cmd=$(echo "[" $comm "]")
         fi  
 
-        printf "%5d %-8s %-6s %1.2f %-20s\n" $item $tty $stat $cpu_usage "$cmd"
+        #printf "%5d %-8s %-6s %1.2f %-20s\n" $item $tty $stat $proc_total_time "$cmd"
+        printf "%5d %-8s %-6s%9s %-20s\n" $item $tty $stat $(convertsecs $proc_total_time) "$cmd"
+
     fi
 done
 
